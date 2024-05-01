b9844c4dca152fe935efc8bfcbc888e3288cdc33
Kiran Kumar <kiransrinivasan03@gmail.com>
2024-05-01 18:09:53 +0530

    added styling ✨

--- client/src/axios/axios.js
+++ client/src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3002",
+  baseURL: "http://localhost:3003",
 });
 
 export default axiosInstance;
--- client/src/components/Stock/Stock.jsx
+++ client/src/components/Stock/Stock.jsx
@@ -1,17 +1,19 @@
 import { useEffect, useState } from "react";
 import axiosInstance from "../../axios/axios";
 import { Link } from "react-router-dom";
+import "./Stock.css"
 
 function Stock() {
+
+
   const [stockData, setStockData] = useState([]);
 
   const fetchData = async () => {
     try {
-      // localhost:3002/stocks
+      // localhost:3003/stocks
       const data = await axiosInstance.get("/stocks");
       console.log(data.data.data);
       setStockData(data.data.data);
-
       console.log(data);
     } catch (error) {
       console.log(error);
@@ -24,26 +26,30 @@ function Stock() {
 
   return (
     <>
-      <section className="py-5">
-        <div className="2xl:container mx-auto">
-          <div className="w-[90%] mx-auto grid grid-cols-1 md:grid-cols-4">
+      <section className="py-5" >
+
+        <div className="2xl:container mx-auto ">
+          <div className="w-[90%] mx-auto grid grid-cols-1 md:grid-cols-3 h-[100vh]">
             {stockData.map((e) => {
               return (
-                <>
-                  <Link to={`/stocks/${e.stock_id}`}>
-                    <div className="bg-slate-50 ring-1 shadow-md max-w-[200px] p-5 ring-slate-400">
-                      <p>{e.name}</p>
-                      <img src={e.image_url} alt={e.name} className="h-9 w-9" />
-                      <p>{e.price}</p>
-                      <p>{e.recent_selling_price}</p>
+                <Link to={`/stocks/${e.stock_id}`} key={e.stock_id}>
+                  <div className="shadow-2xl drop-shadow-md max-w-[200px] max-h-[500px] rounded-lg overflow-hidden bg-white hover:slide-fwd-center">
+                    <img src={e.image_url} alt={e.name} className="w-full" />
+                    <div className="p-4">
+                      <p className="text-xl font-medium text-[#23153c]">{e.name}</p>
+                      <div className="flex items-start gap-4 mt-2">
+                        <p className="text-base">&#8377;{e.price}</p>
+                        <p className="text-base text-[#07B79C]">&#8377;{e.recent_selling_price}</p>
+                      </div>
                     </div>
-                  </Link>
-                </>
+                  </div>
+                </Link>
               );
             })}
           </div>
-        </div>{" "}
+        </div>
       </section>
+
     </>
   );
 }
